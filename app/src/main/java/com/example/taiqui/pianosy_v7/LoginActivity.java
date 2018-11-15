69e76b0337aba6e1cb7f35fc9aae40ddf7be8080
taiqui14 <1512697@hcmut.edu.vn>
2018-11-15 17:00:16 +0700

    Add login with Facebook

--- app/src/main/java/com/example/taiqui/pianosy_v7/LoginActivity.java
+++ app/src/main/java/com/example/taiqui/pianosy_v7/LoginActivity.java
@@ -1,14 +1,45 @@
 package com.example.taiqui.pianosy_v7;
 
+import android.support.annotation.NonNull;
+import android.support.v13.view.inputmethod.InputConnectionCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.internal.WebDialog;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnSignup;
+    CallbackManager callbackManager;
+    LoginButton loginButton;
+    FirebaseAuth auth;
+    TextView t2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +48,65 @@ public class LoginActivity extends AppCompatActivity {
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               setContentView(R.layout.activity_create_account);
+                setContentView(R.layout.activity_create_account);
+            }
+        });
+        callbackManager = CallbackManager.Factory.create();
+
+        buttonclickloginFb();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("email"));
+        t2 = (TextView) findViewById(R.id.tv2);
+    }
+
+    public void buttonclickloginFb() {
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "User cancelled it", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
+
+    private void handleFacebookToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser myuser = auth.getCurrentUser();
+                            updateUI(myuser);
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Could not register to Firebase", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int resquestCode, int resultCode, Intent data)
+    {
+        callbackManager.onActivityResult(resquestCode,resultCode, data);
+        super.onActivityResult(resquestCode,resultCode, data);
+    }
+
+    private void updateUI(FirebaseUser myuser) {
+        t2.setText(myuser.getEmail());
+    }
+
 }
